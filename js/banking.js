97d6eeaaf0adcc2346699def9ca18803f34a5115
MariamSiddika <mariamsiddika81726@gmail.com>
2022-09-15 02:33:11 +0600

    error handler added

--- js/banking.js
+++ js/banking.js
@@ -15,11 +15,18 @@ function updateTotalField(totalFieldId, amount) {
 
     totalElement.innerText = previousTotal + amount;
 }
-
-function updateBalance(amount, isAdd) {
+function getCurrentBalance() {
     const balanceTotal = document.getElementById('balance-total');
     const balanceTotalText = balanceTotal.innerText;
     const previousBalanceTotal = parseFloat(balanceTotalText);
+    return previousBalanceTotal;
+}
+function updateBalance(amount, isAdd) {
+    const balanceTotal = document.getElementById('balance-total');
+    // const balanceTotalText = balanceTotal.innerText;
+
+    //const previousBalanceTotal = parseFloat(balanceTotalText);
+    const previousBalanceTotal = getCurrentBalance();
 
     if (isAdd == true) {
         balanceTotal.innerText = previousBalanceTotal + amount;
@@ -49,8 +56,11 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     balanceTotal.innerText = previousBalanceTotal + depositAmount; */
 
     const depositAmount = getInputValue('deposit-input');
-    updateTotalField('deposit-total', depositAmount);
-    updateBalance(depositAmount, true);
+    
+    if (depositAmount > 0) {
+        updateTotalField('deposit-total', depositAmount);
+        updateBalance(depositAmount, true);
+    }
 });
 
 //Handle withdraw button
@@ -71,7 +81,12 @@ document.getElementById('withdraw-button').addEventListener('click', function ()
     balanceTotal.innerText = previousBalanceTotal - withdrawAmount; */
 
     const withdrawAmount = getInputValue('withdraw-input');
-    updateTotalField('withdraw-total', withdrawAmount);
-    updateBalance(withdrawAmount, false);
-
+    const currentBalance = getCurrentBalance();
+    if (withdrawAmount > 0 && withdrawAmount < currentBalance) {
+        updateTotalField('withdraw-total', withdrawAmount);
+        updateBalance(withdrawAmount, false);
+    }
+    if(withdrawAmount > currentBalance){
+        console.log('Insufficient Balance!!')
+    }
 });
